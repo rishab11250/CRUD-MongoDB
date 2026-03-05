f3d19aa9502e9be47ac8d9b553bd8fd16f01386b
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-03-05 11:26:36 +0530

    added the port for the .env.example

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
