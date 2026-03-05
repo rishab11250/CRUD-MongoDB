d54eb4d5ea1bd34906b8235afb016d63455ce110
rishab11250 <rishab.chandgothia.cg@gmail.com>
2026-03-05 11:25:44 +0530

    Added the backend

---
+++ server.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/task')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Could not connected to MongoDB', err));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+// post one 
+app.post('/one', async (req,res)=>{
+    let user = new User(req.body);
+    await user.save()
+    res.status(201).send({
+        message: 'User created!',
+        user: user
+    });
+})
+
+// post many
+app.post('/many', async (req,res)=>{
+    try{
+
+        let users = new User(req.body);
+        await User.insertMany(req.body)
+        res.status(201).send('Users created!');
+    }
+    catch(err){
+        res.status(500).send('Error creating users!');
+    }
+})
+
+// get all
+app.get('/users', async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.status(200).send(users);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+// get by id
+app.get('/users/:id', async (req,res)=>{
+    let userId = Number(req.params.id);
+    try{
+        const user = await User.findById(userId);
+        res.status(200).send(user);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+// put route
+app.put('/users/:id', async (req,res)=>{
+    let userId = (req.params.id);
+    try{
+        const user = await User.findByIdAndUpdate(userId, req.body);
+        res.status(200).send({
+            message: 'User updated!',
+            user: user
+        });
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+// delete route
+app.delete('/users/:id', async (req,res)=>{
+    let userId = (req.params.id);
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.status(200).send({
+            message: 'User deleted!',
+            user: user
+        });
+    }
+    catch(err){
+        res.status(500).send(err);
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ', PORT);
+});
